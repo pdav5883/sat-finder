86098dc7e460929448d187d2811e330f8c105135
Peter Davison <davison.pl@gmail.com>
2022-09-24 19:57:40 -0400

    Working test device pointing with quaternion lib and calibrate button

--- scripts/point.js
+++ scripts/point.js
@@ -1,10 +1,57 @@
 window.onload = checkStatus
 window.addEventListener("deviceorientation", handleOrientation, true)
+var rad = Math.PI / 180
+var current_alpha = 0
+var alpha_offset = 0
 
 function handleOrientation(event) {
-  document.getElementById("cellA").innerHTML = Math.round(event.alpha)
+  current_alpha = event.alpha
+
+  document.getElementById("cellA").innerHTML = Math.round(correctAlpha(event.alpha))
   document.getElementById("cellB").innerHTML = Math.round(event.beta)
   document.getElementById("cellC").innerHTML = Math.round(event.gamma)
+  //var q = euler_to_quaternion(event.alpha, event.beta, event.gamma).toVector()
+  //document.getElementById("cellA").innerHTML = Math.round(q[0]*1000)/1000
+  //document.getElementById("cellB").innerHTML = Math.round(q[1]*1000)/1000
+  //document.getElementById("cellC").innerHTML = Math.round(q[2]*1000)/1000
+  //document.getElementById("cellD").innerHTML = Math.round(q[3]*1000)/1000
+  
+  var v = eulerToVec(correctAlpha(event.alpha), event.beta, event.gamma)
+  document.getElementById("cellD").innerHTML = Math.round(v[0]*1000)/1000
+  document.getElementById("cellE").innerHTML = Math.round(v[1]*1000)/1000
+  document.getElementById("cellF").innerHTML = Math.round(v[2]*1000)/1000
+  
+  var ae = eulerToAzEl(correctAlpha(event.alpha), event.beta, event.gamma)
+  document.getElementById("cellG").innerHTML = Math.round(ae[0])
+  document.getElementById("cellH").innerHTML = Math.round(ae[1])
+}
+
+
+function calibrateAlpha() {
+  alpha_offset = current_alpha
+}
+
+
+function correctAlpha(alpha) {
+  return (alpha - alpha_offset) % 360.0
+}
+
+function eulerToQuaternion(alpha, beta, gamma) {
+  var q = Quaternion.fromEuler(alpha * rad, beta * rad, gamma * rad, 'ZXY')
+  return q
+}
+
+function eulerToAzEl(alpha, beta, gamma) {
+  var v = eulerToVec(alpha, beta, gamma)
+  var el = Math.asin(v[2]) / rad
+  var az = Math.atan2(v[0], v[1]) / rad
+  return [az, el]
+}
+
+function eulerToVec(alpha, beta, gamma) {
+  var q = eulerToQuaternion(alpha, beta, gamma)
+  var v = q.rotateVector([0, 0, -1])
+  return v
 }
 
 function checkStatus() {
